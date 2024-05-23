94fea3638557f06c18e46f04ea700fec611ae6e7
jlgalavi <jlgalavi@etsinf.upv.es>
2024-05-23 20:34:13 +0200

    fixed footer and update photos boxes

--- js/products.js
+++ js/products.js
@@ -3,28 +3,28 @@ const products = [
         id: 1,
         title: 'Box S',
         price: 100,
-        img: 'images/image.png',
+        img: 'images/caja_s.png',
         size: "15 x 12 x 10"
     },
     {
         id: 2,
         title: 'Box M',
         price: 200,
-        img: 'images/image.png',
+        img: 'images/caja_m.png',
         size: '20 x 21 x 7'
     },
     {
         id: 3,
         title: 'Box L',
         price: 300,
-        img: 'images/image.png',
+        img: 'images/caja_l.png',
         size: '20 x 21 x 21'
     },
     {
         id: 4,
         title: 'Box XL',
         price: 400,
-        img: 'images/image.png',
+        img: 'images/caja_xl.png',
         size: '25 x 30 x 21'
     }
 ];
